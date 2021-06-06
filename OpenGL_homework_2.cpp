2372ce6b548ec62bd421f02e169c3f07e9f61b19
Kvar_ispw17 <enkerewpo@gmail.com>
2021-06-06 16:58:41 +0800

    writing code along with tutorial - Kvar_ispw17

--- OpenGL_homework_2.cpp
+++ OpenGL_homework_2.cpp
@@ -15,17 +15,27 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 600;
 const unsigned int SCR_HEIGHT = 512;
 
-float v[] = {
-    -0.5f, -0.5f, 0.0f,
-    0.5f, -0.5, 0.0f,
-    -0.5f, 0.5f, 0.0f,
-    0.5f, 0.5f, 0.0f
-}; // I want to draw a square !!
 
-const char* vertexShaderSource = 
-"#version 330 core\n"
+const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
-"void main() { gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\0"; // ??? can't we write GLSL outside cpp ???
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
+
+const char* fragmentShaderSource_blue = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(102./255f, 204./255f, 1.f, 1.0f);\n"
+"}\0";
 
 int main()
 {
@@ -53,18 +63,106 @@ int main()
         return -1;
     }
 
-    unsigned int VBO; // vertex buffer object
-    glGenBuffers(1, &VBO); // give id "VBO" a buffer! 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO); // bind "VBO" as CURRENT array buffer
-    glBufferData(GL_ARRAY_BUFFER, sizeof(v), v, GL_STATIC_DRAW); // send v to VBO which is the CURRENT array buffer
+
+
+    // vertex shader compiling
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    else {
+        std::cout << "SUCCESS::SHADER::VERTEX::COMPILATION_SUCCESSED" << std::endl;
+    }
+
+    // fragment shader compiling
+
+    unsigned int mion; // object;
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource_blue, NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    else {
+        std::cout << "SUCCESS::SHADER::FRAGMENT::COMPILATION_SUCCESSED" << std::endl;
+    }
+
+    // shader program linking
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" << infoLog << std::endl;
+    }
+    else {
+        std::cout << "SUCCESS::SHADER::PROGRAM::LINK_SUCCESSED" << std::endl;
+    }
+
+    float v[] = {
+         0.5f,  0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f
+    };
+
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+
+    unsigned int VBO, VAO, EBO;
+    glGenBuffers(1, &VBO); 
+    glGenVertexArrays(1, &VAO);
+  
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+   
+    glBufferData(GL_ARRAY_BUFFER, sizeof(v), v, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+   
+    glEnableVertexAttribArray(0);
+
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+
+    // loop circle
 
     while (!glfwWindowShouldClose(window))
     {
-
         processInput(window);
-
-        glClearColor(1, 1, 1, 1);
+        glClearColor(244. / 255, 255. / 255, 255. / 255, 1);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -73,6 +171,12 @@ int main()
     glfwTerminate();
     return 0;
 }
+
+
+
+
+
+
 
 
 void processInput(GLFWwindow* window)
