a301931cacf8af7de369e3cf84a1c3a5034ff9df
Mion <moe@qwq.cc>
2021-06-22 22:58:34 +0800

    Cute Kvar GOOOOOOOOOOOOOOOOOOOOOOOD night!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- OpenGL_homework_2.cpp
+++ OpenGL_homework_2.cpp
@@ -32,7 +32,7 @@ glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
 Camera camera(glm::vec3(0.0f, 0.0f, 5.0f));
 bool firstMouse = true;
-float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+     float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
 float pitch = 0.0f;
 float lastX = 800.0f / 2.0;
 float lastY = 600.0 / 2.0;
